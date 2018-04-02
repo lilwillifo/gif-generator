d13d77c9427103c6560703cd524b040e4ab12349
lilwillifo <margaretwilliford4@gmail.com>
2018-04-02 14:48:22 -0600

    seed file for gifs

--- spec/features/gifs/user_favorites_gifs_spec.rb
+++ spec/features/gifs/user_favorites_gifs_spec.rb
@@ -5,18 +5,28 @@ require 'rails_helper'
       category = create(:category)
       gif_1 = create(:gif, category: category)
       gif_2 = create(:gif, category: category)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      visit '/login'
+      visit gifs_path
 
-      fill_in 'Username', with: user.username
-      fill_in 'Password', with: user.password
-      click_on 'Log In'
-
-      click_on 'Gifs'
       expect(page).to have_xpath("//img[contains(@src,'#{gif_1.image_path}')]")
       expect(page).to have_xpath("//img[contains(@src,'#{gif_2.image_path}')]")
     end
     it 'and mark their favorites to see in their favorites list' do
+      user = create(:user)
+      category = create(:category)
+      gif_1 = create(:gif, category: category)
+      gif_2 = create(:gif, category: category)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
+      within ".#{gif_1.image_path}" do
+        find('far fa-heart').click
+      end
+
+      expect(page).to have_content('Added to your favorite list!')
+
+      click_on 'Favorites'
+
+      expect(page).to have_xpath("//img[contains(@src,'#{gif_1.image_path}')]")
     end
   end
