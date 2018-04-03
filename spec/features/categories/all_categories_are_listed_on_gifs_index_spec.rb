806c7ecf5097877de59fed49cdf2840c71281c44
lilwillifo <margaretwilliford4@gmail.com>
2018-04-02 20:11:03 -0600

    update seed file and create test for user seeing all categories

---
+++ spec/features/categories/all_categories_are_listed_on_gifs_index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+describe 'A logged-in user' do
+  it 'can see all categories' do
+    user = create(:user)
+    category_1, category_2, category_3,  = create_list(:category, 3)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit gifs_path
+
+    expect(page).to have_content(category_1.name)
+    expect(page).to have_content(category_2.name)
+    expect(page).to have_content(category_3.name)
+  end
+end
