3024f4fb0c5f66bd68f515cc65d2016f14dbaaf1
lilwillifo <margaretwilliford4@gmail.com>
2018-04-04 13:55:19 -0600

    factory bot fixes

--- spec/features/categories/user_can_visit_category_show_page_spec.rb
+++ spec/features/categories/user_can_visit_category_show_page_spec.rb
@@ -2,17 +2,15 @@ require 'rails_helper'
 describe 'A logged-in user' do
   it 'link to category show page from gif index page' do
     user = create(:user)
-    category_1, category_2, category_3 = create_list(:category, 3)
+    gifs = create_list(:gif, 3)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit gifs_path
 
-    click_on "#{category_1.name}"
+    click_on "#{gifs.first.category.name}"
 
-    expect(current_path).to eq(category_path(category_1))
-    expect(page).to have_content(category_1.name)
-    expect(page).to_not have_content(category_2.name)
-    expect(page).to_not have_content(category_3.name)
+    expect(current_path).to eq(category_path(gifs.first.category))
+    expect(page).to have_content(gifs.first.category.name)
   end
   it 'can see all gifs within a category' do
     user = create(:user)
--- spec/features/categories/user_sees_all_categories_spec.rb
+++ spec/features/categories/user_sees_all_categories_spec.rb
@@ -2,17 +2,15 @@ require 'rails_helper'
 describe 'A logged-in user' do
   it 'can see all categories on gif index page' do
     user = create(:user)
-    category_1, category_2, category_3 = create_list(:category, 3)
-    category_1.gifs = create_list(:gif, 2)
-    category_2.gifs = create_list(:gif, 2)
-    category_3.gifs = create_list(:gif, 2)
+    gifs = create_list(:gif, 3)
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit gifs_path
 
-    expect(page).to have_content(category_1.name)
-    expect(page).to have_content(category_2.name)
-    expect(page).to have_content(category_3.name)
+    gifs.each do |gif|
+      expect(page).to have_content(gif.category.name)
+    end
   end
   it 'can see all their categories on favorite page' do
     user = create(:user)
